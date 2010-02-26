b6ac2647e2ae2d93a901868d46e76a09988e147c
liquidsnake@sapo.pt <liquidsnake@sapo.pt@da8fb158-e395-11de-a668-6fd11605f5ce>
2010-02-26 19:21:20 +0000

    This commit is just to send to the repository some batch commands that launch demos.

--- trunk/runscripts/smartrun.bat
+++ trunk/runscripts/smartrun.bat
@@ -1,3 +1,1 @@
-
-java -cp bin\smart-v0.4.jar;lib\slf4j-api-1.5.8.jar;lib\slf4j-jdk14-1.5.8.jar;lib\netty-3.1.1.GA.jar %1 %2 %3 %4 %5 %6 %7
-
+java -cp dist\smart.jar;lib\slf4j-api-1.5.8.jar;lib\slf4j-jdk14-1.5.8.jar;lib\netty-3.1.1.GA.jar %1 %2 %3 %4 %5 %6 %7
