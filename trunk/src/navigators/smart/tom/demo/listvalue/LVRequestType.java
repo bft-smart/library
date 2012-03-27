a9231a1ed56ffca19bd83b442c5beb97d864040a
liquidsnake@sapo.pt <liquidsnake@sapo.pt@da8fb158-e395-11de-a668-6fd11605f5ce>
2012-03-27 17:49:05 +0000

    Added a new demo named "BFTList", which is very similar to "BFTMap", but uses Lists instead of Maps.

---
+++ trunk/src/navigators/smart/tom/demo/listvalue/LVRequestType.java
@@ -0,0 +1,16 @@
+package navigators.smart.tom.demo.listvalue;
+
+public class LVRequestType {
+
+	static final int LIST_CREATE = 1;
+	static final int LIST_REMOVE = 2;
+	static final int SIZE_TABLE = 3;
+	static final int PUT = 4;
+	static final int GET = 5;
+	static final int SIZE = 6;
+	static final int REMOVE = 7;
+	static final int CHECK = 8;
+	static final int GET_LIST = 9;
+	static final int LIST_CREATE_CHECK = 10;
+
+}
