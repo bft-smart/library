46dd3888c5f21554e14ddcd9a1a368d3822dfb7c
liquidsnake@sapo.pt <liquidsnake@sapo.pt@da8fb158-e395-11de-a668-6fd11605f5ce>
2012-04-02 15:43:07 +0000

    trying out this

--- src/navigators/smart/communication/client/ReplyListener.java
+++
@@ -1,9 +0,0 @@
-package navigators.smart.communication.client;
-
-import navigators.smart.tom.core.messages.TOMMessage;
-
-
-public interface ReplyListener {
-
-	public void replyReceived(TOMMessage reply);
-}
