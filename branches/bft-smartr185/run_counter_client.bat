d3f20ac177cdf5fdfe65bd42ecf43047b7505220
marcelmhs@gmail.com <marcelmhs@gmail.com@da8fb158-e395-11de-a668-6fd11605f5ce>
2012-04-07 16:07:02 +0000

    Removed unused scripts.

--- branches/bft-smartr185/run_counter_client.bat
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -cp lib\netty-3.1.1.GA.jar;lib\slf4j-jdk14-1.5.8.jar;lib\slf4j-api-1.5.8.jar;bin\SMaRt_com_Vista.jar navigators.smart.tom.demo.CounterClient %1 %2 %3 %4 %5 %6 %7 %8 %9
