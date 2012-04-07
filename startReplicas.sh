0e34ff93a705a9a2851822bf604d7700d6a60d7e
marcelmhs@gmail.com <marcelmhs@gmail.com@da8fb158-e395-11de-a668-6fd11605f5ce>
2012-04-07 16:07:02 +0000

    Removed unused scripts.

--- startReplicas.sh
+++
@@ -1,7 +0,0 @@
-gnome-terminal --window --maximize -x sh -c "java -cp bin/BFT-SMaRt.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-jdk14-1.5.8.jar:lib/netty-3.1.1.GA.jar:lib/commons-codec-1.5.jar navigators.smart.tom.demo.keyvalue.BFTMapImpl 0"
-sleep 1;
-gnome-terminal --window --maximize -x sh -c "java -cp bin/BFT-SMaRt.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-jdk14-1.5.8.jar:lib/netty-3.1.1.GA.jar:lib/commons-codec-1.5.jar navigators.smart.tom.demo.keyvalue.BFTMapImpl 1"
-sleep 1;
-gnome-terminal --window --maximize -x sh -c "java -cp bin/BFT-SMaRt.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-jdk14-1.5.8.jar:lib/netty-3.1.1.GA.jar:lib/commons-codec-1.5.jar navigators.smart.tom.demo.keyvalue.BFTMapImpl 2"
-sleep 1;
-gnome-terminal --window --maximize -x sh -c "java -cp bin/BFT-SMaRt.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-jdk14-1.5.8.jar:lib/netty-3.1.1.GA.jar:lib/commons-codec-1.5.jar navigators.smart.tom.demo.keyvalue.BFTMapImpl 3"
