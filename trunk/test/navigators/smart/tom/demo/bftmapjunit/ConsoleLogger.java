21d9a4ee67a1225381c2b462d4f78f5bbf7c75aa
liquidsnake@sapo.pt <liquidsnake@sapo.pt@da8fb158-e395-11de-a668-6fd11605f5ce>
2012-04-23 14:36:29 +0000

    Copied more Marcel's bug fix to main trunk.

---
+++ trunk/test/navigators/smart/tom/demo/bftmapjunit/ConsoleLogger.java
@@ -0,0 +1,46 @@
+package navigators.smart.tom.demo.bftmapjunit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class ConsoleLogger extends Thread {
+
+	// Testing commit on branch
+	private InputStream in = null;
+	private PrintStream out = null;
+	private String index;
+
+	public InputStream getIn() {
+		return in;
+	}
+	public void setIn(InputStream in) {
+		this.in = in;
+	}
+	public PrintStream getOut() {
+		return out;
+	}
+	public void setOut(PrintStream out) {
+		this.out = out;
+	}
+	public String getIndex() {
+		return index;
+	}
+	public void setIndex(String index) {
+		this.index = index;
+	}
+	
+	public void run() {
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(in));
+		String s;
+		try {
+			while ((s = stdInput.readLine()) != null) {
+				out.println("Replica " + index + ")" +s);
+			}
+		} catch(IOException ioe) {
+			System.out.println("----------- Exception writing replica log: " + ioe.getMessage());
+		}
+	}
+}
