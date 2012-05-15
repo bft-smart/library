e7d0735e90a953c1777435f1a0fe4ddfef698fc1
marcelmhs@gmail.com <marcelmhs@gmail.com@da8fb158-e395-11de-a668-6fd11605f5ce>
2012-05-15 09:53:59 +0000

    - Removed javadocs from the project because the javadocs had old references to navigators.smart package; - Renamed tests source code to use package bftsmart.

---
+++ trunk/test/bftsmart/tom/demo/bftmap/ConsoleLogger.java
@@ -0,0 +1,45 @@
+package bftsmart.tom.demo.bftmap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class ConsoleLogger extends Thread {
+
+	private InputStream in = null;
+	private PrintStream out = null;
+	private String index;
+
+	public InputStream getIn() {
+		return in;
+	}
+	public void setIn(InputStream in) {
+		this.in = in;
+	}
+	public PrintStream getOut() {
+		return out;
+	}
+	public void setOut(PrintStream out) {
+		this.out = out;
+	}
+	public String getIndex() {
+		return index;
+	}
+	public void setIndex(String index) {
+		this.index = index;
+	}
+	
+	public void run() {
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(in));
+		String s;
+		try {
+			while ((s = stdInput.readLine()) != null) {
+				out.println("Replica " + index + ")" +s);
+			}
+		} catch(IOException ioe) {
+			System.out.println("----------- Exception writing replica log: " + ioe.getMessage());
+		}
+	}
+}
