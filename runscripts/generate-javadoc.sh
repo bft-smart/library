9eb92c67d7614f92ff6f44f8753212c5ccc7cbb0
João Sousa <jcs@lasige.di.fc.ul.pt>
2018-09-29 00:45:43 +0100

    Created new script to generate javadoc

---
+++ runscripts/generate-javadoc.sh
@@ -0,0 +1,1 @@
+javadoc -cp src -d doc bftsmart.tom bftsmart.tom.server src/bftsmart/tom/util/KeyLoader.java src/bftsmart/tom/util/Extractor.java src/bftsmart/tom/server/defaultservices/DefaultRecoverable.java src/bftsmart/tom/server/defaultservices/DefaultSingleRecoverable.java src/bftsmart/tom/server/defaultservices/durability/DurabilityCoordinator.java
