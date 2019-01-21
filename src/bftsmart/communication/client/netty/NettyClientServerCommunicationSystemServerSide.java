64227047b3436fb761194fe2f9614d30dbcd935c
João Sousa <jcs@lasige.di.fc.ul.pt>
2019-01-21 20:25:20 +0000

    correction 2

--- src/bftsmart/communication/client/netty/NettyClientServerCommunicationSystemServerSide.java
+++ src/bftsmart/communication/client/netty/NettyClientServerCommunicationSystemServerSide.java
@@ -266,6 +266,8 @@ public class NettyClientServerCommunicationSystemServerSide extends SimpleChanne
 	@Override
 	public void send(int[] targets, TOMMessage sm, boolean serializeClassHeaders) {
                             
+                listener.waitForChannels(sending); // wait for the previous transmission to complete
+            
                 sending = 0;
                 
 		//serialize message
@@ -371,9 +373,7 @@ public class NettyClientServerCommunicationSystemServerSide extends SimpleChanne
 			} finally {
 				rl.readLock().unlock();
 			}
-		}
-
-                listener.waitForChannels(sending); // wait for the this transmission to complete
+		}                
 
 	}
 
