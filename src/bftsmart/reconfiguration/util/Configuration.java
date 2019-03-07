53857f1e96fe4d295b4b6034dc785d0317ffc947
João Sousa <jcs@lasige.di.fc.ul.pt>
2019-03-07 20:21:03 +0000

    Small fix

--- src/bftsmart/reconfiguration/util/Configuration.java
+++ src/bftsmart/reconfiguration/util/Configuration.java
@@ -189,8 +189,8 @@ public class Configuration {
             }
             
             s = (String) configs.remove("system.totalordermulticast.prooftype");
-            if(s == null && !s.equalsIgnoreCase("macvector") && !s.equalsIgnoreCase("signatures")){
-                proofType = "signatures";
+            if(s == null || !s.equalsIgnoreCase("macvector") || !s.equalsIgnoreCase("signatures")){
+                proofType = "macvector";
             }else{
                 proofType = s;
             }
