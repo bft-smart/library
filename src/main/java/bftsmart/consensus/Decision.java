f5fe3d860583e0279b029d3723dc2d7502634979
Christian Berger <cb@sec.uni-passau.de>
2022-01-13 16:25:50 +0100

    Fix possible nullpointer in waitForPropose()

--- src/main/java/bftsmart/consensus/Decision.java
+++ src/main/java/bftsmart/consensus/Decision.java
@@ -126,7 +126,7 @@ public class Decision {
     }
 
     private void waitForPropose() {
-        while (decisionEpoch == null &&
+        while (decisionEpoch == null ||
                 decisionEpoch.deserializedPropValue == null) {
             try {
                 LoggerFactory.getLogger(this.getClass()).info("waiting for propose for consensus " + cid);
