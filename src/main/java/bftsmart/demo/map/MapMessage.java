1d851331c389e5b892d80093f4bc6c6f7673dac3
tiagorncarvalho <tiagorncarvalho@gmail.com>
2023-03-16 14:09:04 +0000

    Changed message serialization

--- src/main/java/bftsmart/demo/map/MapMessage.java
+++ src/main/java/bftsmart/demo/map/MapMessage.java
@@ -2,6 +2,7 @@ package bftsmart.demo.map;
 
 import java.io.*;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class MapMessage<K,V> implements Serializable {
@@ -17,7 +18,36 @@ public class MapMessage<K,V> implements Serializable {
     public static <K,V> byte[] toBytes(MapMessage<K,V> message) throws IOException {
         ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
         ObjectOutputStream objOut = new ObjectOutputStream(byteOut);
-        objOut.writeObject(message);
+        boolean hasType = message.getType() != null;
+        objOut.writeBoolean(hasType);
+        if(hasType) {
+            objOut.writeObject(message.getType());
+        }
+        boolean hasKey = message.getKey() != null;
+        objOut.writeBoolean(hasKey);
+        if(hasKey) {
+            objOut.writeObject(message.getKey());
+        }
+        boolean hasValue = message.getValue() != null;
+        objOut.writeBoolean(hasValue);
+        if(hasValue) {
+            objOut.writeObject(message.getValue());
+        }
+        boolean hasSize = message.getSize() > 0;
+        objOut.writeBoolean(hasSize);
+        if(hasSize) {
+            objOut.writeInt(message.getSize());
+        }
+        boolean hasKeySet = message.getKeySet() != null;
+        objOut.writeBoolean(hasKeySet);
+        if(message.getKeySet() != null) {
+            Set<K> keySet = message.getKeySet();
+            int size = keySet.size();
+            objOut.writeInt(size);
+            for (K key : keySet)
+                objOut.writeObject(key);
+        }
+
 
         objOut.flush();
         byteOut.flush();
@@ -29,7 +59,34 @@ public class MapMessage<K,V> implements Serializable {
     public static <K,V> MapMessage<K,V> fromBytes(byte[] rep) throws IOException, ClassNotFoundException {
         ByteArrayInputStream byteIn = new ByteArrayInputStream(rep);
         ObjectInputStream objIn = new ObjectInputStream(byteIn);
-        return (MapMessage<K,V>) objIn.readObject();
+        MapMessage<K,V> msg = new MapMessage<>();
+        boolean hasType = objIn.readBoolean();
+        if(hasType) {
+            msg.setType((MapRequestType) objIn.readObject());
+        }
+        boolean hasKey = objIn.readBoolean();
+        if(hasKey) {
+            msg.setKey(objIn.readObject());
+        }
+        boolean hasValue = objIn.readBoolean();
+        if(hasValue) {
+            msg.setValue(objIn.readObject());
+        }
+        boolean hasSize = objIn.readBoolean();
+        if(hasSize) {
+            msg.setSize(objIn.readInt());
+        }
+        boolean hasKeySet = objIn.readBoolean();
+        if(hasKeySet) {
+            int setSize = objIn.readInt();
+            Set<K> result = new HashSet<>();
+            while (setSize-- > 0) {
+                result.add((K)objIn.readObject());
+            }
+            msg.setKeySet(result);
+        }
+
+        return msg;
     }
 
     public MapRequestType getType() {
