e0399ca0a2e5eed7b30c1b093b3f82d7250332af
njs97 <nuriajalalisantos@gmail.com>
2023-07-07 12:08:54 +0100

    changes for pull request

--- src/main/java/bftsmart/tests/IntegrationTestsExecutor.java
+++ src/main/java/bftsmart/tests/IntegrationTestsExecutor.java
@@ -4,15 +4,17 @@ import bftsmart.tests.recovery.RecoveryTest;
 
 public class IntegrationTestsExecutor {
 	public static void main(String[] args) throws Exception {
-		if (args.length != 2) {
-			throw new IllegalArgumentException("USAGE: bftsmart.tests.IntegrationTestsExecutor <working directory> <f>");
+		if (args.length != 3) {
+			throw new IllegalArgumentException("USAGE: bftsmart.tests.IntegrationTestsExecutor <working directory> <f> <isbft>");
 		}
+		System.out.println("aaa");
 		String workingDirectory = args[0];
 		int f = Integer.parseInt(args[1]);
+		boolean isbft = Boolean.parseBoolean(args[2]);
 		long start, end, delta;
 		System.out.println("Running recovery test");
 		start = System.nanoTime();
-		AbstractIntegrationTest recoveryTest = new RecoveryTest(workingDirectory, f);
+		AbstractIntegrationTest recoveryTest = new RecoveryTest(workingDirectory, f, isbft);
 		recoveryTest.executeTest();
 		end = System.nanoTime();
 		System.out.println("Recovery test terminated");
